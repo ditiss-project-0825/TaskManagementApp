2ea48cc196b9f41cafacd48603227a969d97ec58
aryan2bamane <aryanbamane2@gmail.com>
2026-02-08 11:29:52 +0530

    changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,11 +60,9 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_instance" "devsecops" {
   ami           = "ami-019715e0d74f695be"
   instance_type = "m7i-flex.large"
-  key_name      = "one"   # <-- your key pair name
+  key_name      = "one"
 
-  vpc_security_group_ids = [
-    aws_security_group.ec2_sg.id
-  ]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   root_block_device {
     volume_size = 25
@@ -84,14 +82,13 @@ output "public_ip" {
 }
 
 # -----------------------------
-# Generate Ansible Inventory
+# Write Ansible Inventory
 # -----------------------------
 resource "null_resource" "write_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-mkdir -p ../ansible
 echo "[jenkins]" > ../ansible/inventory.ini
-echo "${aws_instance.devsecops.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/one" >> ../ansible/inventory.ini
+echo "${aws_instance.devsecops.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=/home/ad/.ssh/one" >> ../ansible/inventory.ini
 EOT
   }
 
